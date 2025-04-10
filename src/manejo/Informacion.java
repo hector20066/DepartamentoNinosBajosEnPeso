32dbe280715b11ac7ab7e7f5178af189fbf3df07
hector20066 <hectorluisacevedocontreras00@gmail.com>
2025-04-10 08:04:39 -0500

    Se agregaron los metodos setters de la clase Informacion, Por favor agregar los metodos getters

--- src/manejo/Informacion.java
+++ src/manejo/Informacion.java
@@ -26,4 +26,63 @@ public class Informacion {
 
     }
 
+    public void setNombreNino(String nombreNino){
+        this.nombreNino = nombreNino;
+    }
+
+    public void setIdentificacionNino(int identificacionNino){
+        this.identificacionNino = identificacionNino;
+    }
+
+    public void setEdad(int edad){
+        this.edad = edad;
+    }
+
+    public void setEstatura(int estatura){
+        this.estatura = estatura;
+    }
+
+    public void setPeso(int peso){
+        this.peso = peso;
+    }
+
+    public void setNumRegistroCivil(int numRegistroCivil){
+        this.numRegistroCivil = numRegistroCivil;
+    }
+
+    public void setMunicipio(String municipio){
+        this.municipio = municipio;
+    }
+
+    public void setNomRepresentante(String nomRepresentante){
+        this.nomRepresentante = nomRepresentante;
+    }
+
+    public void setParentesco(String parentesco){
+        this.parentesco = parentesco;
+    }
+
+    public void setIdRepresentante(int idRepresentante){
+        this.idRepresentante = idRepresentante;
+    }
+
+    public void setSiguiente(Informacion Siguiente){
+        this.siguiente = siguiente;
+    }
+
+    //Metodos getters
+
+    public void copiarNodo(Informacion nodo){
+        this.nombreNino = nodo.getNombreNino;
+        this.identificacionNino = nodo.getIdentificacionNino;
+        this.edad = nodo.getEdad;
+        this.estatura = nodo.getEstatura;
+        this.peso = nodo.getPeso;
+        this.numRegistroCivil = nodo.getNumRegistroCivil;
+        this.municipio = nodo.getMunicipio;
+        this.nomRepresentante = nodo.getNomRepresentante;
+        this.parentesco = nodo.getParentesco;
+        this.idRepresentante = nodo.getIdRepresentante;
+    }
+
 }
